4f40c9529e4768fb9b11043708dba29b869ea731
Alp Tahta <alp.tht48@gmail.com>
2025-01-21 09:02:49 +0300

    Complete item end point with path parameter

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("test")
+	port := ":8080"
+
+	router := http.NewServeMux()
+
+	// Path Parameter
+	router.HandleFunc("/item/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Write([]byte("Recieved request for item id : " + id))
+	})
+
+	s := &http.Server{
+		Addr:           port,
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("Starting server on Port%s\n", port)
+	log.Fatal(s.ListenAndServe())
 }
