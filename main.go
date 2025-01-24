eae5424506af499a9b20962c7c8818d925fea899
Alp Tahta <alp.tht48@gmail.com>
2025-01-24 14:34:35 +0300

    Edit API for user service

--- main.go
+++ main.go
@@ -1,36 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
 	port := ":8080"
 
+	userAPI := "http://localhost:8080"
+
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		hostname, err := os.Hostname()
+	router.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	router.HandleFunc("GET /greet/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+
+		requestURL := fmt.Sprintf("%s/%s/%s", userAPI, "user", id)
+		res, err := http.Get(requestURL)
 		if err != nil {
 			log.Println(err)
-			http.Error(w, "Failed to get hostname", http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		_, err = w.Write([]byte("Hostname : " + hostname + time.Now().String()))
+
+		if res.StatusCode != http.StatusOK {
+			log.Printf("Response code from userAPI is %d\n", res.StatusCode)
+			http.Error(w, fmt.Sprintf("Response Code From userAPI is %d\n", res.StatusCode), http.StatusInternalServerError)
+			return
+		}
+
+		resBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write(append([]byte("Hi "), resBody...))
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "Could not write data", http.StatusInternalServerError)
 			return
 		}
-	})
-
-	// Path Parameter
-	router.HandleFunc("GET /item/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		w.Write([]byte("Recieved request for item id : " + id))
 	})
 
 	s := &http.Server{
